ddd3a9607056ecd592cf5807a0c960152341f3ed
Tiago Fujii <tiagoyukio12@gmail.com>
2018-09-08 15:32:48 -0300

    Save results to output file
    Save cost, execution time, and route taken in output file

--- EP1_9350011.R
+++ EP1_9350011.R
@@ -1,11 +1,13 @@
 EP1_9350011 <- function(arqEntrada, arqSaida) {
     source('D:/las_descargas/github-projects/PCS3438-EP1/vizProx.R')
     source('D:/las_descargas/github-projects/PCS3438-EP1/calcCusto.R')
-    
-    # EP1_9350011('D:/las_descargas/github-projects/PCS3438-EP1/entrada.csv','foo')
+
+    tInicial <- Sys.time()
+
+    # EP1_9350011('D:/las_descargas/github-projects/PCS3438-EP1/entrada.csv','D:/las_descargas/github-projects/PCS3438-EP1/saida.csv')
     matAdj <- as.matrix(read.table(arqEntrada, sep = ',', header=TRUE))
 
-    # greedy algorithm
+    # Algoritmo Nearest Neighbour
     menorCusto <- Inf
     melhorRota <- -1;
     matViz <- matrix(-1, ncol(matAdj), ncol(matAdj))
@@ -25,9 +27,9 @@ EP1_9350011 <- function(arqEntrada, arqSaida) {
 
 
 
-    # save file
-    print(melhorRota)
-    print(menorCusto)
-
-
+    # Imprime resultado e salva no arquivo de saida
+    tExec <- Sys.time() - tInicial
+    cat("Custo: ", menorCusto, "\n Tempo de Execucao: ", tExec, "\n Rota: ", melhorRota, "\n")
+    resultado <- c(menorCusto, tExec, melhorRota)
+    write.table(resultado, file = arqSaida, sep = ",", row.names = FALSE, col.names = FALSE)
 }
